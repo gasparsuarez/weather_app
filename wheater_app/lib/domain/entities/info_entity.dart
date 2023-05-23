c625ddc69a8906ec915ab7cdc0411d13d3ff88ba
Gaspar Suarez <gaspar.mdp@hotmail.com>
2023-05-23 10:09:00 -0300

    Test changes

---
+++ wheater_app/lib/domain/entities/info_entity.dart
@@ -0,0 +1,9 @@
+class InfoEntity {
+  final String location;
+  final String country;
+  final String region;
+  final double tempC;
+
+  InfoEntity(
+      {required this.location, required this.country, required this.region, required this.tempC});
+}
